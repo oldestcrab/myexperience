31ac598d423130f162404bd1098300fca0a03b64
oldestcrab <oldestcrab@gmail>
2018-06-25 16:19:52 +0800

    测试调用chrome driver

--- test/test.py
+++ test/test.py
@@ -1,12 +1,3 @@
-# -*- coding:utf-8 -*-
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(n, '等于', x, '*', n//x)
-            break
-    else:
-        # 循环中没有找到元素
-        print(n, ' 是质数')
-    if n == 8:
-        break
-    print("当前是:" + str(n))
+from selenium import webdriver
+
+browser = webdriver.Chrome()
