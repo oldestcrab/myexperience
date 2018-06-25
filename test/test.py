36f1d93bc96ef123066350c77b78f8152cdbe45a
oldestcrab <oldestcrab@gmail>
2018-06-25 15:55:59 +0800

    测试

---
+++ test/test.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, '等于', x, '*', n//x)
+            break
+    else:
+        # 循环中没有找到元素
+        print(n, ' 是质数')
+    if n == 8:
+        break
+    print("当前是:" + str(n))
