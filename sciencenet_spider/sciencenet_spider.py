ac00ddfa96fc05cf29077f8c305562ddb330f5fb
oldestcrab <oldestcrab@gmail>
2018-06-26 16:47:20 +0800

    判断已增加，测试通过，差细节修改

--- sciencenet_spider/sciencenet_spider.py
+++ sciencenet_spider/sciencenet_spider.py
@@ -9,6 +9,7 @@ from lxml import etree
 import json
 import time
 import requests
+import re
 #import os
 
 browser = webdriver.PhantomJS(executable_path=r'C:/Users/CRAB/Desktop/mybooks/execute/phantomjs-2.1.1-windows/bin/phantomjs.exe')
@@ -17,14 +18,19 @@ browser = webdriver.PhantomJS(executable_path=r'C:/Users/CRAB/Desktop/mybooks/ex
 wait = WebDriverWait(browser, 10)
 
 
-def index_page(page):
+def index_page(page, judge):
     """
     抓取索引页
     :param page: 页码
     """
     print('正在爬取第', page, '页索引页')
     try:
-        url = 'http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2'
+        #url_list = ['http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2','http://news.sciencenet.cn/fieldlist.aspx?id=3']
+        #for url in url_list:
+        # 论文链接
+        #url = 'http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2'
+        # 领域新闻链接
+        url = 'http://news.sciencenet.cn/fieldlist.aspx?id=3'
         browser.get(url)
         if page > 1:
             #print('test')
@@ -39,38 +45,53 @@ def index_page(page):
             submit.click()
             #time.sleep(1)
         wait.until(EC.text_to_be_present_in_element_value((By.NAME, 'AspNetPager1_input'), str(page)))
-        get_products()
+        return get_products(page, judge)
     except TimeoutException:
         print('爬取第', page, '页索引页time out,尝试重新爬取!')
-        index_page(page)
+        index_page(page, judge)
         
 
 
-def get_products():
+def get_products(page, judge):
     """
     提取文章内容
     """
     html = browser.page_source
+    #print(type(html))
     #with open('./html.html', 'a', encoding = 'utf-8') as f:
     #    f.write(html)
     result = etree.HTML(html)
     items = result.xpath('//*[@id="DataGrid1"]/tbody//tbody')
     #print(items)
     #print(type(items))
+    if page == 1:
+        #领域新闻为 @width = "60%" ， 论文为 @width = "60%"
+        next_judge = items[0].xpath('.//td[@width = "60%"]/a/@href')[0]
+        with open('sciencenet_spider/judge.text', 'w', encoding = 'utf-8') as f:
+            print(next_judge)
+            f.write(next_judge)
     for item in items:
         #print(type(item))
         #title = item.xpath('./tr[2]/td[1]/a')[0].text
         #print(title,type(title))
         #content = {
-        #    'title': item.xpath('.//td[@width = "70%"]/a')[0].text.replace('\n','').replace(' ',''),
-        #    'url': item.xpath('.//td[@width = "70%"]/a/@href')[0],
+        #    'title': item.xpath('.//td[@width = "60%"]/a')[0].text.replace('\n','').replace(' ',''),
+        #    'url': item.xpath('.//td[@width = "60%"]/a/@href')[0],
         #    'from': item.xpath('.//td[@width = "20%"]')[0].text.replace('\n','').replace(' ',''),
         #    'date': item.xpath('.//td[@width = "10%"]')[0].text.replace('\n','').replace(' ','')
         #}
         #print(content)
-        kw = item.xpath('.//td[@width = "70%"]/a/@href')[0]
+        kw = item.xpath('.//td[@width = "60%"]/a/@href')[0]
+        #title = item.xpath('.//td[@width = "60%"]/a')[0].text.replace('\n','').replace(' ','')
+        if kw == judge:
+            print("爬取到上次爬取位置，结束爬取！")
+            return 0
+            break
         #print(type(kw))
         url = 'http://paper.sciencenet.cn' + kw
+        #/htmlpaper/201861510484322846535.shtm
+        filename_pattern = re.compile('[a-zA-Z./]')
+        filename = filename_pattern.sub('', kw)
         #print(url)
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
         }
@@ -80,32 +101,43 @@ def get_products():
         #print(detail_html.encoding,detail_html.apparent_encoding)
         #print(detail_html.text)
         #detail_html = etree.HTML(response.text)
-        save_to_json(response)
+        save_to_json(response.text, filename)
+    
+    
 
 
-def save_to_json(result):
+def save_to_json(result, filename):
     """
     保存到文件
     :param result: 结果
     """
     #with open('./result.json', 'a', encoding = 'utf-8') as f:
     #    f.write(json.dumps(result, indent = 2, ensure_ascii = False))
-    detail_html = etree.HTML(result.text)
-    detail_content = detail_html.xpath('//*[@id = "content"]')
-    print(type(detail_content))
-    print(detail_content)
-    #detail_result = etree.tostring(detail_content)
-    #with open('./test.html', 'w', encoding = 'utf-8') as f:
-    #    f.write(deatil_result.decode('utf-8'))
-    #print('done')    
+    #print(result)
+    detail_pattern = re.compile(r'<table id="content".*<!-- JiaThis Button END -->.*?</table>',re.S)
+    detail_result = detail_pattern.search(result)
+    #print(type(detail_html))
+    #detail_content = etree.tostring(detail_html)
+    #detail_result = detail_html.xpath('//table[@id="content"]')
+    #print(type(detail_result))
+    #print(detail_result.group())
+    full_filename = './sciencenet_spider/sciencenet_spider_result/' + filename + '.html'
+    with open(full_filename, 'w', encoding = 'utf-8') as f:
+        f.write(detail_result.group())
+    print('文件' + full_filename + '保存成功')    
 
 def main():
     """
     遍历每一页索引页
     """
-    for i in range(1, 2):
+    with open('sciencenet_spider/judge.text', 'r', encoding = 'utf-8') as f:
+        judge = f.read()
+    for i in range(1, 4):
         #print(i)
-        index_page(i)
+        params = index_page(i, judge)
+        if params == 0:
+            break
+        
         
     browser.close()
 
--- test/test.py
+++ test/test.py
@@ -1,3 +1,6 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome()
+import re
+# /htmlpaper/201861510484322846535.shtm
+pattern = re.compile('[a-zA-Z./]')
+a = '/htmlpaper/201861510484322846535.shtmhtmlnews/2018/6/414034.shtm'
+b = pattern.sub('',a)
+print(b.group(1))
