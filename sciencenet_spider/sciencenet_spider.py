5dc2a9e66ed8d6fbeed34c26c2b11e2bcf03d496
oldestcrab <oldestcrab@gmail>
2018-06-28 17:01:41 +0800

    图片已添加，测试尚未完成

--- sciencenet_spider/sciencenet_spider.py
+++ sciencenet_spider/sciencenet_spider.py
@@ -9,6 +9,7 @@ from lxml import etree
 import json
 import time
 import requests
+from requests.exceptions import ConnectionError
 import re
 #import os
 
@@ -29,9 +30,9 @@ def index_page(page, judge):
         #url_list = ['http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2','http://news.sciencenet.cn/fieldlist.aspx?id=3']
         #for url in url_list:
         # 论文链接
-        #url = 'http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2'
+        url = 'http://paper.sciencenet.cn/paper/fieldlist.aspx?id=2'
         # 领域新闻链接
-        url = 'http://news.sciencenet.cn/fieldlist.aspx?id=3'
+        #url = 'http://news.sciencenet.cn/fieldlist.aspx?id=3'
         browser.get(url)
         if page > 1:
             input = wait.until(
@@ -60,9 +61,9 @@ def get_pages(page, judge):
     items = result.xpath('//*[@id="DataGrid1"]/tbody//tbody')
     if page == 1:
         #领域新闻为 @width = "60%" ， 论文为 @width = "70%"
-        next_judge = items[0].xpath('.//td[@width = "60%"]/a/@href')[0]
-        with open('sciencenet_spider/judge.text', 'w', encoding = 'utf-8') as f:
-            print(next_judge)
+        next_judge = items[0].xpath('.//td[@width = "70%"]/a/@href')[0]
+        with open('sciencenet_spider/judge.txt', 'w', encoding = 'utf-8') as f:
+            print("next_judge:\t" + next_judge)
             f.write(next_judge)
     for item in items:
         #content = {
@@ -71,7 +72,7 @@ def get_pages(page, judge):
         #    'from': item.xpath('.//td[@width = "20%"]')[0].text.replace('\n','').replace(' ',''),
         #    'date': item.xpath('.//td[@width = "10%"]')[0].text.replace('\n','').replace(' ','')
         #}
-        kw = item.xpath('.//td[@width = "60%"]/a/@href')[0]
+        kw = item.xpath('.//td[@width = "70%"]/a/@href')[0]
         #title = item.xpath('.//td[@width = "60%"]/a')[0].text.replace('\n','').replace(' ','')
         if kw == judge:
             print("爬取到上次爬取位置，结束爬取！")
@@ -79,7 +80,7 @@ def get_pages(page, judge):
             break
         url = 'http://paper.sciencenet.cn' + kw
         #/htmlpaper/201861510484322846535.shtm
-        filename_pattern = re.compile('[a-zA-Z./]')
+        filename_pattern = re.compile(r'[a-zA-Z:./-_]')
         filename = filename_pattern.sub('', kw)
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
         }
@@ -88,9 +89,49 @@ def get_pages(page, judge):
         time.sleep(2)
         #print(detail_html.encoding,detail_html.apparent_encoding)
         detail_pattern = re.compile(r'<table id="content".*<!-- JiaThis Button END -->.*?</table>',re.S)
-        detail_result = detail_pattern.search(response.text)        
-        save_page(detail_result, filename)
-        print('保存第', page, '页索引页所有文章成功') 
+        detail_search = detail_pattern.search(response.text)   
+        detail_result = detail_search.group()
+        # 匹配url链接
+        img_pattern = re.compile(r'<img(.*?)src="(.*?)"', re.S)
+        # 获取所有的url链接
+        img_findall = img_pattern.findall(detail_result)
+        #print("test1")
+        for detail in img_findall:
+            #print("test2")
+            #print(detail[1], type(detail[1]))
+            try:
+                img_sour_pattern = re.compile(r'http')
+                img_judge = img_sour_pattern.search(detail[1])
+                if img_judge is None:
+                    img_url = 'http://news.sciencenet.cn' + detail[1] 
+                else:
+                    img_url = detail[1] 
+                print(img_url)
+                img_response = requests.get(img_url, headers = headers).content
+                img_save_name = filename_pattern.sub('', detail[1])
+                #print('img_save_name:' + img_save_name)
+                save_img(img_response, img_save_name)
+            except ConnectionError:
+                print('图片网址有误:' + url)
+        # img_url_name = './img/' + img_save_name + '.jpg'
+        # img_url_name = img_pattern.sub('', detail_result)
+        
+        def img_url_name(match):
+            img_url_pattern = re.compile(r'[a-zA-Z:/.-_]')
+            img_url_detail = img_url_pattern.sub('', match.group(2))
+            img_url_detail_add = img_url_pattern.search(match.group(1))
+            #print("img_url_detail:" + img_url_detail)
+            #print('img_url_detail_add: ' + img_url_detail_add.group(), type(img_url_detail_add))
+            #if img_url_detail_add is None: 
+            img_name = '<img src="./img/' + img_url_detail + '.jpg'
+            #else:
+            #    img_name = '<img ' + img_url_detail_add.group() + 'src="./img/' + img_url_detail + '.jpg"'
+            print("img_name:" + img_name)
+            return img_name
+        real_result = img_pattern.sub(img_url_name, detail_result)
+            
+        save_page(real_result, filename)
+    print('保存第', page, '页索引页所有文章成功') 
     
     
 
@@ -103,16 +144,26 @@ def save_page(result, filename):
     """
     full_filename = './sciencenet_spider/sciencenet_spider_result/' + filename + '.html'
     with open(full_filename, 'w', encoding = 'utf-8') as f:
-        f.write(result.group())
+        f.write(result)
     #print('文件' + full_filename + '保存成功')    
+
+def save_img(result, filename):
+    """
+    保存文章中的图片
+    :param result: 图片文件
+    :param filename: 保存的图片名
+    """
+    img_save_full_name = './sciencenet_spider/sciencenet_spider_result/img/' + filename + '.jpg'
+    with open(img_save_full_name, 'wb') as f:
+        f.write(result)       
 
 def main():
     """
     遍历每一页索引页
     """
-    with open('sciencenet_spider/judge.text', 'r', encoding = 'utf-8') as f:
+    with open('sciencenet_spider/judge.txt', 'r', encoding = 'utf-8') as f:
         judge = f.read()
-    for i in range(1, 4):
+    for i in range(1, 3):
         params = index_page(i, judge)
         if params == 1:
             break
--- test/test.py
+++ test/test.py
@@ -1,7 +1,180 @@
 import re
 # /htmlpaper/201861510484322846535.shtm
-pattern = re.compile('不(.*?)吧')
-a = '/ht不124356吧mlpaper/201861510484322846535.shtmhtmlnews/2018/6/4140不dsgds好吧34.shtm'
-b = pattern.sub('好吧',a)
+#pattern = re.compile('[a-zA-Z./]')
+patterns = re.compile('<img src="(.*?).jpg', re.S)
+a = '''
+<table id="content" style="border: solid 1px #bbc89b; width: 680px; text-align: center;
+
+                                background-color: #f8f9f4">
+
+                                <tr>
+
+                                    <td align="left">
+
+                                        <div style="height: 20px; margin: 0 auto; border-bottom: solid 1px #bbc89b; margin-top: 10px;
+
+                                            width: 82%; font: 12px 宋体; float: left; ">
+
+                                            作者：晋楠 来源：<a href="http://www.sciencenet.cn/dz/dznews_photo.aspx" class="highlight2">中国科学报</a> 发布时间：2018/6/20 9:18:03</div>
+
+                                        <div style="height: 20px; margin: 0 auto; border-bottom: solid 1px #bbc89b; margin-top: 10px;
+
+                                            width: 18%; font: 12px 宋体; float: right">
+
+                                            选择字号：<span onclick="showsize(12)" style="background-color: #587c19; color: #fff;
+
+                                                font-size: 12px; cursor: pointer">小</span> <span onclick="showsize(14)" style="background-color: #587c19;
+
+                                                    color: #fff; font-size: 12px; cursor: pointer">中</span> <span onclick="showsize(16)"
+
+                                                        style="background-color: #587c19; color: #fff; font-size: 12px; cursor: pointer">
+
+                                                        大</span>
+
+                                        </div>
+
+                                    </td>
+
+                                </tr>
+
+                                <tr>
+
+                                    <td align="center">
+
+                                        <div id="content1" style=" padding:15px; text-align: left; line-height: 24px;
+
+                                            word-wrap: break-word" class="f14">
+
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+
+                                                <tr>
+
+                                                    <td height="20">
+
+                                                    </td>
+
+                                                </tr>
+
+                                                <tr>
+
+                                                    <td align="center" valign="middle" class="style1" style="font-size: 13px; color: #333333;
+
+                                                        font-family: 宋体; line-height: 20px">
+
+                                                        <b></b></td>
+
+                                                </tr>
+
+                                                <tr>
+
+                                                    <td align="center" class="style1" style="font-size: 22px; color: #587c19; font-family: 黑体;
+
+                                                        line-height: 30px">
+
+                                                        世界杯期间俄生化学家“被放假”</td>
+
+                                                </tr>
+
+                                                <tr>
+
+                                                    <td align="center" valign="middle" class="style1" style="font-size: 13px; color: #333333;
+
+                                                        font-family: 宋体; line-height: 20px">
+
+                                                        <b></b></td>
+
+                                                </tr>
+
+                                            </table>
+
+                                            <br />
+
+<p align="center">
+
+	<img src="/upload/news/images/2018/6/2018620530352120.jpg" /></p>
+
+<p>
+
+	&nbsp;</p>
+
+<p style="text-indent: 2em; text-align: center">
+
+	世界杯前采取的安保措施对一些俄罗斯科学家产生了影响。<strong>图片来源： Olga Maltseva/AFP</strong></p>
+
+<p style="text-indent: 2em">
+
+	全世界足球迷都在关注世界杯。而一些俄罗斯研究人员发现他们看比赛的时间比自己预期的要多。</p>
+
+<p style="text-indent: 2em">
+
+	《自然》杂志采访了俄罗斯相关分子生物学家和生物化学家，他们表示，由于该国政府在世界杯前采取的安全和反恐措施，一些俄罗斯实验室无法获得其迫切需要的放射性试剂。</p>
+
+<p style="text-indent: 2em">
+
+	在5月11日发布的总统令中，俄罗斯政府以安全考虑为由，将危险化学和生物物质（包括有毒和放射性化学物质）的销售和运输暂停两个月。世界杯将持续到7月15日。莫斯科附近斯科尔科沃科技学院生物化学家Konstantin Severinov表示，该法令仅适用于举办比赛的城市，但包括莫斯科在内的许多城市恰好是研究中心。</p>
+
+<p style="text-indent: 2em">
+
+	Severinov说，这些措施可能会阻碍俄罗斯相对较少的分子生物学研究。今年5月下了放射性核甘酸（用于测量基因表达和其他实验）订单的俄罗斯研究人员接到了来自莫斯科俄罗斯科学院有机化学研究所的坏消息：因为法令要求，预计6月交付给他们实验室的供给品将被取消。而俄罗斯其他研究中心不能提供这种试剂。</p>
+
+<p style="text-indent: 2em">
+
+	&ldquo;这让我的实验室的整个工作处于困境。&rdquo;同时担任莫斯科俄罗斯科学院分子遗传学和基因生物学研究所课题组长的Severinov说。他表示，包括CRISPR-Cas9基因编辑实验和测量毒素对细胞影响的许多项目都受到了影响。</p>
+
+<p style="text-indent: 2em">
+
+	圣彼得堡杜布赞斯基基因组生物信息学中心主任Stephen O &rsquo;Brien说，他所在团队的工作主要涉及计算，因此并未受到影响。但他从其他研究所的同事那里听说，他们在获得放射性试剂和其他有毒化学物质方面遇到了困难<strong>。（晋楠）</strong></p>
+
+<div style="text-indent:2em;color: #587c19">
+
+	《中国科学报》 (2018-06-20 第3版 国际)</div>
+
+
+
+                                           
+
+                                            <div style="border-bottom: solid 1px #bfc89d; vertical-align: bottom; width: 100%;
+
+                                                height: 20px">
+
+                                            </div>
+
+                                            <div style="width: 100%; height: 20px; text-align: center; margin:10px">
+
+                                            <!-- JiaThis Button BEGIN -->
+
+<script src="/html/js/share.js" type="text/javascript"></script>
+
+<!-- JiaThis Button END -->
+
+                                            </div>
+
+                                            
+
+                                        </div>
+
+                                    </td>
+
+                                </tr>
+
+
+
+                            </table>'''
+j = re.compile('<img src="(.*?).jpg"', re.S)
+img_findall = j.findall(a)
+print(img_findall)
+
+def test(match):
+    #print(match.group(0))
+    #print(match.group(1))
+    pattern = re.compile('[a-zA-Z/]')
+    c = pattern.sub('', match.group(1))
+    d = '<img src="./img/' + c
+    print(d)
+    return d
+b = patterns.sub(test, a)
+
 #print(b.group(1))
-print(b)
+#print(b)
+
+
