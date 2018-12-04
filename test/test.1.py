c03e78bc08a2813f13ef677796e5feefe4334e83
oldestcrab <oledestcrab@gmail.com>
2018-12-04 17:20:55 +0800

    测试通过，还未增加第二版

--- test/test.1.py
+++ test/test.1.py
@@ -1,7 +1,7 @@
 import json
 import sys
-a = [ 1, '23', '小红', '小明']
-with open(sys.path[0] + '/123.json', 'w', encoding = 'utf-8') as f:
-    f.write(json.dumps(a, indent = 2, ensure_ascii= False))
-b = json.load(open(sys.path[0] + '/123.json', 'r', encoding = 'utf-8'))
-print(b)
+a = ''
+if :
+    print('sdjglsj')
+else:
+    print('s')
