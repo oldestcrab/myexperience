9b7fc57c914717bbf427ae2bb59ceaf423f7b9c5
oldestcrab <oledestcrab@gmail.com>
2019-02-12 15:44:10 +0800

    增加登陆认证

---
+++ WorkSpider/spider/cookiespool/generator.py
@@ -0,0 +1,6 @@
+import json
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+from setting import *
+from db import RedisClient
+from login.weibo.cookies import WeiboCookies
