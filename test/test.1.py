b4e2fcb3b5f24c6dd73394afca64eff33ec794b0
oldestcrab <oledestcrab@gmail.com>
2019-03-27 16:53:33 +0800

    测试csv

--- test/test.1.py
+++ test/test.1.py
@@ -6,3 +6,5 @@ a = 'CTLA-4 (polymorphism) [HSA:1493] [KO:K06538]'
 print(re.search(r'(.*?)\[.*\]', a).group(1))
 c = re.sub(r'\(.*\)','',re.search(r'(.*?)\[.*\]', a).group(1))
 print(c)
+
+['11-BETA-HSD3,100174880,"Abnormalities, Drug-Induced",MESH:D000014,,Endocrine Disruptors,5.16,,22659286']
