8699e452601b8933a425a4d49aa78c748e5d7cd7
oldestcrab <oledestcrab@gmail.com>
2019-03-28 16:53:50 +0800

    编写中

--- MySpider/other/reddit_articles.py
+++ MySpider/other/reddit_articles.py
@@ -7,7 +7,7 @@ import json
 
 def parse_index():
     """
-    解析文章json数据
+    解析index
     """
     headers ={
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -32,13 +32,27 @@ def parse_index():
         # with open(r'C:/Users/CRAB/Desktop/gonewildstories.json', 'r', encoding='utf-8') as f:
         #     gws_dict = json.load(f)
         for i in gws_dict['posts'].keys():
-            # global after
-            # after = i
-            # print(i)
             articles_dict = gws_dict['posts'][i]
             # print(articles_dict['title'])
-            # print(articles_dict['permalink'])
-        print('==============')
+            article_url = articles_dict['permalink']
+            parse_article(article_url)
+
+def parse_article(article_url):
+    """
+    解析文章
+    """
+    headers ={
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+    response = requests.get(article_url, headers=headers)
+    
+    
 
 if __name__ == '__main__':
     print('start\t', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
